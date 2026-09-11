8a4d082afbe1f96cadf905b3adf38b82c23bb776
Tarun Panchal <tarunpanchal047@gmail.com>
2026-09-11 08:00:13 +0530

    11

--- React/src/Navbar/Navbar.jsx
+++ React/src/Navbar/Navbar.jsx
@@ -1,36 +1,64 @@
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
+import { Fragment } from 'react';
 
 const Navbar = () => {
+  let user_id = localStorage.getItem(userId)
+  const [data, SetData] = useState([])
+
+
   return (
-    <nav className="navbar" aria-label="Main navigation">
 
-      <ul className="navbar__links">
 
+    < nav className="navbar" aria - label= "Main navigation" >
+
+    <ul className="navbar__links">
+
+
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      ( user_id ? (
+      <Fragment>
         <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
 
-        <li>
-          <NavLink to="/AllProduct">AllProducts</NavLink>
           <NavLink to="/Cart">Cart</NavLink>
         </li>
-
         <li>
-          <NavLink to="/EditProfile">EditProfile</NavLink>
-          <NavLink to="/Delete">DeleteProfile</NavLink>
-          <NavLink to="/Logout">Logout</NavLink>
+          <NavLink to="/AllProduct">AllProducts</NavLink>
         </li>
 
-        <li>
-          <NavLink to="/Login">Login</NavLink>
-          <NavLink to="/SignUp">SignUp</NavLink>
-        </li>
 
-      </ul>
+        <ul>
+            <li>
+              <NavLink to="/EditProfile">EditProfile</NavLink>
+              </li>
 
-    </nav>
-  );
+            <li><NavLink to="/Delete">DeleteProfile</NavLink></li>
+            <li>
+              <NavLink to="/Logout">Logout</NavLink>
+            </li>
+          </ul>
+          <Fragment />
+          ): (
+
+  <Fragment>
+<ul>
+    <li>
+      <NavLink to="/Login">Login</NavLink>
+
+    </li>
+    <li>
+      <NavLink to="/SignUp">SignUp</NavLink>
+    </li>
+       </ul >
+  </Fragment>
+)
+
+     
+
+      </nav >
+      );
 };
 
 export default Navbar;
